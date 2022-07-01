717b69b81cb2c1a192e621bdae816dfb9fd6af02
Ehsan Sarshar <ehsan.herai@gmail.com>
2022-07-01 18:32:00 +0430

    generate random boxes

--- src/phaser/scenes/Game.ts
+++ src/phaser/scenes/Game.ts
@@ -13,18 +13,23 @@ const LINE_WIDTH = window.innerWidth / 4
 const ONE_THIRD_OF_SCREEN_HEIGHT = window.innerHeight / 3
 const widthFourthPart = window.innerWidth / 4
 
-const MAXIMUM_ROAD_TOP_Y =
+const MAXIMUM_ROAD_TOP_Y = Math.round(
   ONE_THIRD_OF_SCREEN_HEIGHT - ONE_THIRD_OF_SCREEN_HEIGHT / 3
+)
 
-const MINIMUM_ROAD_TOP_Y = ONE_THIRD_OF_SCREEN_HEIGHT
+const MINIMUM_ROAD_TOP_Y = Math.round(ONE_THIRD_OF_SCREEN_HEIGHT)
 
-const ROAD_HEIGHT = ONE_THIRD_OF_SCREEN_HEIGHT + ONE_THIRD_OF_SCREEN_HEIGHT / 3
+const ROAD_HEIGHT = Math.round(
+  ONE_THIRD_OF_SCREEN_HEIGHT + ONE_THIRD_OF_SCREEN_HEIGHT / 3
+)
 
 export class Game extends Scene {
   helicopter?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
   groupOfCurves?: Phaser.Physics.Arcade.StaticGroup
+  boxes?: Phaser.Physics.Arcade.StaticGroup
   topLines?: Phaser.Curves.Path
   bottomLines?: Phaser.Curves.Path
+  // boxes?: Phaser.GameObjects.Rectangle[]
   moveSpeed: number
   /** used to track how many pixel landscape is moved */
   moveOffset: number
@@ -33,7 +38,7 @@ export class Game extends Scene {
   constructor() {
     super({ key: 'Game' })
     this.moveOffset = 0
-    this.moveSpeed = 1
+    this.moveSpeed = 4
   }
   preload() {
     this.load.spritesheet('helicopter', 'src/assets/helicopter.png', {
@@ -58,6 +63,9 @@ export class Game extends Scene {
       ONE_THIRD_OF_SCREEN_HEIGHT + ONE_THIRD_OF_SCREEN_HEIGHT / 2,
       'helicopter'
     )
+
+    this.boxes = this.physics.add.staticGroup()
+
     this.helicopter.setInteractive()
     this.anims.create({
       key: 'move',
@@ -72,12 +80,15 @@ export class Game extends Scene {
     // Graphics part
     this.drawer = this.add.graphics()
     // gameState.curvesGroup = this.physics.add.staticGroup()
-    this.topLines = new Phaser.Curves.Path()
-    this.bottomLines = new Phaser.Curves.Path()
+    this.topLines = new Phaser.Curves.Path(0, MAXIMUM_ROAD_TOP_Y)
+    this.bottomLines = new Phaser.Curves.Path(
+      0,
+      MAXIMUM_ROAD_TOP_Y + ROAD_HEIGHT
+    )
 
     // generate initial lines
     // consider one more line as buffer
-    for (let x = 0; x <= SCREEN_WIDTH + LINE_WIDTH; x += LINE_WIDTH) {
+    for (let x = LINE_WIDTH; x <= SCREEN_WIDTH + LINE_WIDTH; x += LINE_WIDTH) {
       const topLineY = Phaser.Math.Between(
         MAXIMUM_ROAD_TOP_Y,
         MINIMUM_ROAD_TOP_Y
@@ -86,9 +97,50 @@ export class Game extends Scene {
 
       this.topLines.lineTo(x, topLineY)
       this.bottomLines.lineTo(x, BottomLineY)
+
+      if (x > 2 && Phaser.Math.Between(0, 3) === 1) {
+        this.drawRectangle(x, topLineY)
+      }
     }
 
+    this.physics.add.collider(this.boxes, this.helicopter, () => {
+      this.cameras.main.shake(250, 0.005)
+    })
+
     this.moveOffset = 0
+  }
+
+  drawRectangle(xOffset: number, yOffset: number) {
+    const rectangleHeight = ROAD_HEIGHT / 4
+    const rectangleWidth = LINE_WIDTH / 8
+    const helicopterHeight = this.helicopter!.displayHeight
+
+    const positions = [
+      // top
+      yOffset - rectangleHeight / 10,
+      // middle top
+      yOffset + helicopterHeight * 2,
+      // middle bottom
+      yOffset + helicopterHeight * 5,
+      // bottom
+      yOffset + ROAD_HEIGHT - rectangleHeight,
+    ]
+
+    // pick a random position
+    const [randomYOffset] = positions
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 1)
+
+    const rectangle = this.add
+      .rectangle(
+        xOffset + LINE_WIDTH,
+        randomYOffset,
+        rectangleWidth,
+        rectangleHeight,
+        0xcc183a
+      )
+      .setOrigin(0, 0)
+    this.boxes?.add(rectangle)
   }
 
   update(): void {
@@ -103,30 +155,45 @@ export class Game extends Scene {
       throw new Error('top and bottom lines should not be empty')
     }
 
-    this.cameras.main.x += this.moveSpeed
+    this.cameras.main.scrollX += this.moveSpeed
     this.helicopter.x += this.moveSpeed
     this.moveOffset += this.moveSpeed
 
     if (this.input.activePointer.isDown) {
-      this.helicopter.y -= 2
+      // this.helicopter.y -= 2
       this.helicopter.angle = 5
+      this.helicopter.setVelocityY(-200)
     } else {
-      this.helicopter.y += 2
+      // this.helicopter.y += 2
       this.helicopter.angle = 0
+      this.helicopter.setVelocityY(150)
     }
 
-    if (this.moveOffset >= 200) {
-      console.log('move offset if clause')
+    if (this.moveOffset >= LINE_WIDTH) {
       const topLineY = Phaser.Math.Between(
         MAXIMUM_ROAD_TOP_Y,
         MINIMUM_ROAD_TOP_Y
       )
-      const BottomLineY = topLineY + ROAD_HEIGHT
+
+      const bottomLineY = topLineY + ROAD_HEIGHT
+
+      console.log({
+        MAX_TOP_Y: MAXIMUM_ROAD_TOP_Y,
+        MIN_TOP_Y: MINIMUM_ROAD_TOP_Y,
+        TOP_LINE_Y: topLineY,
+        BOTTOM_LINE_Y: bottomLineY,
+      })
+
+      const endPointX = this.topLines.getEndPoint().x
+
+      if (endPointX > 2 && Phaser.Math.Between(0, 1) === 1) {
+        this.drawRectangle(endPointX, topLineY)
+      }
 
       this.topLines.lineTo(this.topLines.getEndPoint().x + LINE_WIDTH, topLineY)
       this.bottomLines.lineTo(
         this.bottomLines.getEndPoint().x + LINE_WIDTH,
-        BottomLineY
+        bottomLineY
       )
       this.topLines.curves.shift()
       this.bottomLines.curves.shift()
@@ -136,13 +203,9 @@ export class Game extends Scene {
     // clear previous command to avoid necessary redraw
     this.drawer.clear()
 
-    if (this.topLines?.curves.length) {
-      this.drawLandChunk(this.topLines.curves, 'top')
-    }
+    this.drawLandChunk(this.topLines.curves, 'top')
 
-    if (this.bottomLines?.curves.length) {
-      this.drawLandChunk(this.bottomLines.curves, 'bottom')
-    }
+    this.drawLandChunk(this.bottomLines.curves, 'bottom')
   }
 
   drawLandChunk(curves: Phaser.Curves.Curve[], position: 'top' | 'bottom') {
@@ -155,12 +218,15 @@ export class Game extends Scene {
       },
     ]
 
+    let lastX = 0
+
     for (let i = 0; i < curves.length; i++) {
       const eachCurve = curves[i]
       points.push(eachCurve.getStartPoint(), eachCurve.getEndPoint())
+      lastX = eachCurve.getEndPoint().x
     }
 
-    points.push({ x: curves[curves.length - 1].getEndPoint().x, y: yValue })
+    points.push({ x: lastX, y: yValue })
 
     this.drawer!.fillStyle(0xcc183a)
     this.drawer!.fillPoints(points, true, true)
