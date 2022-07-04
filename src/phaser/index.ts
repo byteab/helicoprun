2d874f505659ba45295dcca075cb34f022afa388
Ehsan Sarshar <ehsan.herai@gmail.com>
2022-07-04 19:22:27 +0430

    use windows height

--- src/phaser/index.ts
+++ src/phaser/index.ts
@@ -5,14 +5,17 @@ import { StartMenu } from './scenes/StartMenu'
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: window.innerWidth,
-  height: 600,
+  height: window.innerHeight,
   parent: 'phaser',
-  scene: [StartMenu, Game],
+  // scene: [StartMenu, Game],
+  scene: [Game],
   physics: {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
+      debug: false,
     },
   },
+  backgroundColor: 0xffffff,
 }
 const game = new Phaser.Game(config)
