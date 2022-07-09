3e8ff8a6f62aca9e8bba7205692575dbb55a8af2
Ehsan Sarshar <ehsan.herai@gmail.com>
2022-07-09 12:11:24 +0430

    update start menu

--- src/phaser/scenes/StartMenu.ts
+++ src/phaser/scenes/StartMenu.ts
@@ -1,22 +1,34 @@
 import { Scene } from 'phaser'
+
+const style = {
+  font: 'bold 32px Arial',
+  fill: '#fff',
+  boundsAlignH: 'center',
+  boundsAlignV: 'center',
+}
+
 export class StartMenu extends Scene {
   startTitle?: Phaser.GameObjects.Text
   constructor() {
     super({ key: 'StartMenu' })
   }
   create() {
-    this.add.rectangle(0, 0, window.innerWidth, 600, 0x00ffff).setOrigin(0, 0)
-    this.startTitle = this.add.text(
-      window.innerWidth / 2 - 80,
-      300,
-      'CLICK TO START',
-      {
-        color: '#000000',
-        fontSize: '20px',
-      }
-    )
+    this.add
+      .rectangle(0, 0, window.innerWidth - 10, window.innerHeight, 0x2b0101)
+      .setOrigin(0, 0)
+
+    this.scale.lockOrientation(Phaser.Scale.LANDSCAPE)
+
+    const screenCenterX =
+      this.cameras.main.worldView.x + this.cameras.main.width / 2
+    const screenCenterY =
+      this.cameras.main.worldView.y + this.cameras.main.height / 2
+    this.startTitle = this.add
+      .text(screenCenterX, screenCenterY, 'CLICK TO START', style)
+      .setOrigin(0.5)
     this.startTitle.setInteractive()
     this.startTitle.on('pointerup', () => {
+      this.scale.startFullscreen()
       this.scene.stop('StartMenu')
       this.scene.start('Game')
     })
